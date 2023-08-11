023af0ce88faf0afd72198635ed6c89e40f11813
Vitor Nascimento <bielvitor2008@hotmail.com>
2023-08-11 11:06:35 -0300

    feat: Finish ex04

--- ex04/lib/app/ui/weekly_weather_page.dart
+++ ex04/lib/app/ui/weekly_weather_page.dart
@@ -3,6 +3,8 @@
 import 'package:ex00/app/domain/controllers/weekly_weather_controller.dart';
 import 'package:ex00/app/domain/models/place.dart';
 import 'package:ex00/app/domain/models/weekly_weather.dart';
+import 'package:ex00/app/domain/utils/weather_icon_util.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class WeeklyWeatherPage extends StatefulWidget {
@@ -67,39 +69,209 @@ class _WeeklyWeatherPageState extends State<WeeklyWeatherPage> {
 
   List<Widget> _getInfoWidgets(Place place, BuildContext context) {
     List<Widget> list = [];
+    ThemeData theme = Theme.of(context);
+
     list.add(Text(
       widget._place.name,
       textAlign: TextAlign.center,
-      style: Theme.of(context).textTheme.displayLarge,
+      style: theme.textTheme.displayLarge
+          ?.copyWith(color: theme.colorScheme.primary),
     ));
+
     if (widget._place.region != null) {
-      list.add(Text(
-        widget._place.region!,
-        textAlign: TextAlign.center,
-        style: Theme.of(context).textTheme.displayLarge,
-      ));
+      list.add(Text(widget._place.region!,
+          textAlign: TextAlign.center,
+          style: theme.textTheme.displayMedium
+              ?.copyWith(color: theme.colorScheme.secondary)));
     }
+
     if (widget._place.country != null) {
       list.add(Text(
         widget._place.country!,
         textAlign: TextAlign.center,
-        style: Theme.of(context).textTheme.displayLarge,
+        style: theme.textTheme.displayMedium
+            ?.copyWith(color: theme.colorScheme.secondary),
       ));
     }
 
-    for (int i = 1; i <= 7; i++) {
-      var index = i - 1;
-      list.add(Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text(_weeklyWeather.dates[index]),
-          Text('${_weeklyWeather.minTemperatures[index]}C°'),
-          Text('${_weeklyWeather.maxTemperatures[index]}C°'),
-          Text(_weeklyWeather.weatherDescriptions[index]),
-        ],
-      ));
-    }
+    list.add(const SizedBox(height: 48));
+
+    list.add(AspectRatio(
+      aspectRatio: 1,
+      child: LineChart(
+        LineChartData(
+          lineBarsData: [
+            LineChartBarData(
+                isCurved: true,
+                spots: List.generate(
+                  _weeklyWeather.maxTemperatures.length,
+                  (index) {
+                    return FlSpot(index.toDouble(),
+                        _weeklyWeather.maxTemperatures[index]);
+                  },
+                ),
+                dotData: const FlDotData(show: true),
+                color: theme.colorScheme.primary),
+            LineChartBarData(
+                isCurved: true,
+                spots: List.generate(
+                  _weeklyWeather.minTemperatures.length,
+                  (index) {
+                    return FlSpot(index.toDouble(),
+                        _weeklyWeather.minTemperatures[index]);
+                  },
+                ),
+                dotData: const FlDotData(show: true),
+                color: theme.colorScheme.secondary),
+          ],
+          maxX: 6,
+          minX: 0,
+          maxY: _weeklyWeather.maxTemperatures
+                  .reduce((max, temperature) =>
+                      temperature > max ? temperature : max)
+                  .toInt()
+                  .toDouble() +
+              3,
+          minY: _weeklyWeather.minTemperatures
+                  .reduce((min, temperature) =>
+                      temperature < min ? temperature : min)
+                  .toInt()
+                  .toDouble() -
+              1,
+          clipData: const FlClipData.all(),
+          borderData: FlBorderData(show: true),
+          gridData: const FlGridData(show: true),
+          backgroundColor: theme.colorScheme.primaryContainer,
+          lineTouchData: const LineTouchData(enabled: false),
+          titlesData: FlTitlesData(
+            topTitles: AxisTitles(
+                axisNameSize: 35,
+                axisNameWidget: Text(
+                  "Weekly temperatures",
+                  style: theme.textTheme.displaySmall
+                      ?.copyWith(color: theme.colorScheme.secondary),
+                ),
+                sideTitles: const SideTitles(
+                  showTitles: false,
+                )),
+            bottomTitles: AxisTitles(
+                axisNameSize: 50,
+                axisNameWidget: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        size: 15,
+                        color: theme.colorScheme.primary,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const Text("Max temperature"),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        size: 15,
+                        color: theme.colorScheme.secondary,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      const Text("Min temperature"),
+                    ],
+                  ),
+                ),
+                sideTitles: SideTitles(
+                  showTitles: true,
+                  interval: 1,
+                  getTitlesWidget: (index, meta) {
+                    return SideTitleWidget(
+                      axisSide: meta.axisSide,
+                      child: Text(
+                          _formatDate(_weeklyWeather.dates[index.toInt()])),
+                    );
+                  },
+                )),
+            leftTitles: AxisTitles(
+                sideTitles: SideTitles(
+              reservedSize: 40,
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                return SideTitleWidget(
+                  axisSide: meta.axisSide,
+                  child: Text('${value.toInt()}°C'),
+                );
+              },
+            )),
+            rightTitles: const AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+          ),
+        ),
+      ),
+    ));
+
+    list.add(SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: _getWeatherWidgets(context),
+      ),
+    ));
 
     return list;
+  }
+
+  List<Widget> _getWeatherWidgets(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    var list = <Widget>[const SizedBox(height: 100)];
+    int index = 0;
+    for (var date in _weeklyWeather.dates) {
+      list.add(Material(
+        color: theme.colorScheme.primaryContainer,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                _formatDate(date),
+                style: theme.textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 8),
+              Icon(
+                WeatherIconUtil.getIconFromWeatherDescription(
+                  _weeklyWeather.weatherDescriptions[index],
+                ),
+                color: theme.colorScheme.primary,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '${_weeklyWeather.maxTemperatures[index]}C° max',
+                style: theme.textTheme.titleMedium,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '${_weeklyWeather.minTemperatures[index]}C° min',
+                style: theme.textTheme.titleMedium,
+              ),
+            ],
+          ),
+        ),
+      ));
+      if (index != _weeklyWeather.dates.length - 1) {
+        list.add(const SizedBox(width: 8));
+      }
+      index = index + 1;
+    }
+    return list;
+  }
+
+  String _formatDate(String date) {
+    return date.substring(0, 5).replaceAll('-', '/');
   }
 }
